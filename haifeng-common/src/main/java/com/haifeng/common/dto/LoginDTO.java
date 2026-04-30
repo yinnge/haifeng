f3a50f802d0f1e0e2e301ae3d2df8f6d59cb9352
yinnge <485143018@gmail.com>
2026-04-30 11:01:24 +0800

    feat: 修改 LoginDTO 为手机号登录

--- haifeng-common/src/main/java/com/haifeng/common/dto/LoginDTO.java
+++ haifeng-common/src/main/java/com/haifeng/common/dto/LoginDTO.java
@@ -1,20 +1,18 @@
 package com.haifeng.common.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
-/**
- * 登录请求 DTO
- */
 @Data
 public class LoginDTO {
 
-    @NotBlank(message = "用户名不能为空")
-    @Size(min = 2, max = 50, message = "用户名长度必须在2-50之间")
-    private String username;
+    @NotBlank(message = "手机号不能为空")
+    @Pattern(regexp = "^1[3-9]\\d{9}$", message = "手机号格式不正确")
+    private String phone;
 
     @NotBlank(message = "密码不能为空")
-    @Size(min = 6, max = 100, message = "密码长度必须在6-100之间")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{6,16}$",
+             message = "密码必须是数字+字母，长度6-16位")
     private String password;
 }
