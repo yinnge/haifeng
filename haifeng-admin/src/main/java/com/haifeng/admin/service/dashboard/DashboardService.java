42b646f7f84bede48e1f9b27f5deced7b5b1ca4a
yinnge <485143018@gmail.com>
2026-07-27 17:12:07 +0800

    feat: 创建DashboardService接口

---
+++ haifeng-admin/src/main/java/com/haifeng/admin/service/dashboard/DashboardService.java
@@ -0,0 +1,7 @@
+package com.haifeng.admin.service.dashboard;
+
+import com.haifeng.admin.vo.dashboard.DashboardStatsVO;
+
+public interface DashboardService {
+    DashboardStatsVO getDashboardStats();
+}
